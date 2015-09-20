22906db38b8b8256f874047074b92d5bdb1b5ad2
Gabit Kemelov <kemelov_gabit@mail.ru>
2015-09-20 11:53:04 +0600

    Update TableParser.java

--- TableParser.java
+++ TableParser.java
@@ -7,12 +7,11 @@
 package htmlparser;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 /**
  *
@@ -30,9 +29,18 @@ public class TableParser {
 //            System.out.println("--------------------------------------");
 //            System.out.println(table.select("tbody tr").get(count-1).text()); // get(index) 0<=index<=count-1
             
+            // TODO: последние данные спустя через 2 месяца после 8-го числа
+            
             for(int i=0; i<count; i++) {
-                System.out.println(table.select("tbody tr").get(i).text());
-                System.out.println("------------------------------------------");
+                int n = i+1; // row number
+                String row = table.select("tbody tr").get(i).text(); // row data
+                List<String> list = new ArrayList<>(Arrays.asList(row.split(" ")));
+                int index = list.size()-2; // берем предпоследюю запись для Russia
+                String rus = list.get(index); // это нам и надо!!!
+                System.out.println("Russia's value on row "+n+": "+rus);
+                System.out.println("row "+n+": "+row);
+                System.out.println(" ");
             }
+            
     }
 }
